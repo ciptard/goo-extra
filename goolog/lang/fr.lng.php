1e047182462e554661e5b73d5f6b31ec381f2ca4
Fred89 <fred@mypluxml.com>
2011-11-21 20:44:17 +0100

    revert lang php file

---
+++ goolog/lang/fr.lng.php
@@ -0,0 +1,47 @@
+<?php
+
+$lang['post'] = 'Article';
+$lang['reply'] = 'Commentaire';
+$lang['plugin'] = 'Plugin';
+$lang['more'] = 'Plus...';
+$lang['config'] = 'Configuration';
+$lang['logout'] = 'Déconnexion';
+$lang['login'] = 'Connexion';
+$lang['redirect'] = 'Redirection vers';
+$lang['add'] = 'Ajouter ';
+$lang['edit'] = 'Éditer ';
+$lang['delete'] = 'Supprimer ';
+$lang['title'] = 'Titre&nbsp;&nbsp;&nbsp;&nbsp;';
+$lang['content'] = 'Message';
+$lang['name'] = 'Nom&nbsp;&nbsp;&nbsp;&nbsp;';
+$lang['view'] = 'Vues';
+$lang['search'] = 'Rechercher';
+$lang['link'] = 'Lien';
+$lang['category'] = 'Catégorie';
+$lang['archive'] = 'Archive';
+$lang['url'] = 'URL';
+$lang['page'] = 'Page';
+$lang['prev'] = 'Récent';
+$lang['next'] = 'Ancien';
+$lang['password'] = 'Mot de Passe';
+$lang['poweredBy'] = 'Motorisé par';
+$lang['feed'] = 'Fil';
+$lang['theme'] = 'Thème';
+$lang['lang'] = 'Langue';
+$lang['none'] = 'Aucune Donnée Actuellement';
+$lang['confirm'] = 'Valider';
+$lang['uncategorized'] = 'Non Classé';
+$lang['yes'] = 'Oui';
+$lang['no'] = 'Non';
+$lang['locked'] = 'Fermer&nbsp;&nbsp;&nbsp;&nbsp;';
+$lang['day'] = 'Jour';
+$lang['hour'] = 'heure';
+$lang['minute'] = 'minute';
+$lang['second'] = 'seconde';
+$lang['plural'] = 's';
+$lang['ago'] = 'avant';
+$lang['errorLength'] = ' est trop long ou trop court';
+$lang['errorBot'] = 'CAPTCHA Incorrecte';
+$lang['replied'] = 'répondus à ';
+
+?>

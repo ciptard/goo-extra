38fa9983f80166e9943bc9eb8f7736a22f2fc49b
Fred89 <fred@mypluxml.com>
2012-02-20 01:05:30 +0100

    Backup plugin update to 1.2
    Work but make archive error and don't delete file actually

---
+++ goobbs/plugin/backup/lang/en.lng.php
@@ -0,0 +1,17 @@
+<?php
+$lang['state']              = 'on/off plugin:';
+$lang['state_on']           = 'On';
+$lang['state_off']          = 'Off';
+$lang['make_backup']        = 'Make a backup';
+$lang['backup']             = 'Backup';
+$lang['return_config']      = 'Go to plugin page';
+$lang['confirm_delete']     = 'Confirm delete file?';
+$lang['total_archives']     = 'Total backup files:';
+$lang['archive_error']      = 'Error for make the archive !';
+$lang['archive_success']    = 'Archive make succefull !';
+$lang['view_backup']        = 'View my backup file';
+$lang['download']           = 'Download';
+$lang['action']             = 'Action';
+$lang['backup_folder']      = 'Backup folder name <em>(dont forget to rename this in your ftp !!)</em> :';
+$lang['data_save']          = 'Datas Save !';
+?>

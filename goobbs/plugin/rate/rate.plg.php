62d1e1f1ea0ca36ea4086e134195a0fc7320e336
taylorchu <tailinchu@gmail.com>
2012-05-27 19:25:39 -0700

    clean up rate plugin; improve read speed

--- goobbs/plugin/rate/rate.plg.php
+++ goobbs/plugin/rate/rate.plg.php
@@ -1,53 +1,68 @@
 <?php
 
+if (!isValidEntry('plugin', 'rate'))
+	saveEntry('plugin', 'rate', array());
+$rateEntry = readEntry('plugin', 'rate');
+
+$type = 'topic';
+
 function rate_crit()
 {
 	return array('agree', 'funny', 'useful', 'friendly', 'informative');
 }
 
-function rate_aftertopic($topic)
+function rate_afterTopic($id)
 {
-	if(isGET('id') && isGET('exp') && (isValidEntry('topic', $_GET['id']) || isValidEntry('reply', $_GET['id'])))
+	global $type;
+	if(isGET('id') && isGET('exp') && (isValidEntry($type, $_GET['id']) || isValidEntry('reply', $_GET['id'])))
 	{
 		rate_saveRate($_GET['id'], $_GET['exp']);
 	}
-	return rate_show($topic);
+	return rate_show($id);
 }
 
-function rate_afterReply($reply)
+function rate_afterReply($id)
 {
-	return rate_show($reply);
+	return rate_show($id);
+}
+
+function lock($id)
+{
+	return 'rate_' .$id;
 }
 
 function rate_getRate($id)
 {
-	$rateFile = 'rate_' .$id;
-	if(isValidEntry('plugin', $rateFile))
-	{
-		return readEntry('plugin', $rateFile);
-	}
-	return array_fill_keys (rate_crit(), 0);
+	global $rateEntry;
+	if (isset($rateEntry[$id]))
+		return $rateEntry[$id];
+	else
+		return array_fill_keys (rate_crit(), 0);
 }
 
 function rate_saveRate($id, $exp)
 {
-	$rateFile = 'rate_' .$id;
-	if(isset($_SESSION[$rateFile]))
+	global $rateEntry;
+	$lock = lock($id);
+	if (isset($_SESSION[$lock]))
 		return;
+	
 	$rate = rate_getRate($id);
 	if(isset($rate[$exp]))
 	{
 		$rate[$exp]++;
 	}
-	saveEntry('plugin', $rateFile, $rate);
-	$_SESSION[$rateFile] = $rateFile;
+	$rateEntry[$id] = $rate;
+	saveEntry('plugin', 'rate', $rateEntry);
+	$_SESSION[$lock] = 1;
 }
 
 function rate_show($id)
 {
+	global $type;
 	$rate = rate_getRate($id);
 	$out = '<div class="exp" style="font-size: 9px; margin: 9px 0">';
-	if(isset($_SESSION['rate_' .$id]))
+	if(isset($_SESSION[lock($id)]))
 	{
 		$out .= '<img alt="" src="plugin/rate/img/accept.png"> / ';
 		foreach($rate as $exp => $score)
@@ -59,7 +74,7 @@ function rate_show($id)
 	{
 		foreach($rate as $exp => $score)
 		{
-			$out .= '<a href="view.php/topic/' .$_GET['topic']. '/id/' .$id. '/exp/' .$exp. '"><img alt="" src="plugin/rate/img/' .$exp. '.png"></a> x '. $score. ' / ';
+			$out .= '<a href="view.php/' .$type. '/' .$_GET[$type]. '/id/' .$id. '/exp/' .$exp. '"><img alt="" src="plugin/rate/img/' .$exp. '.png"></a> x '. $score. ' / ';
 		}
 	}
 	$out .= '</div>';
